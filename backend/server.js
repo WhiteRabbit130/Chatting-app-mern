51ee025a43d86cb60b9dba6b130f5a14fb3ecb34
WhiteRabbit130 <whiterabbit.followme130@gmail.com>
2024-03-17 05:37:08 +0200

    update server

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,6 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 
-// app.get("/", (req, res) => {
-//   res.send("API Running!");
-// });
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
